b7e8e29107d66f4c9284ea9d0e5959384cf2e343
Javier Aguilar <mjolnic@gmail.com>
2014-05-17 04:27:02 +0200

    demo routes

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,58 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the Closure to execute when that URI is requested.
+  |
+ */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// When users hit a route without a language,
+// redirect them to the default one using a 302 redirect
+Route::get('/', function() {
+    return Redirect::to(Lang::code(), 302);
+});
+
+// non-multilingual route
+Route::any('/hey/', function() {
+    return 'Hey, I\'m not a multilingual route!';
+});
+
+// specific route in spanish
+Route::any('/es/hola/', function() {
+    return 'Hola mundo!';
+});
+
+// specific route in english
+Route::any('/en/hello/', function() {
+    return 'Hello world!';
+});
+
+// all other routes that share the same path, common in all languages
+Route::langGroup(function() {
+    Route::get('/', function() {
+        return 'Homepage in ' . Lang::code();
+    });
+    Route::get('/foo/', function() {
+        return 'Foo page in ' . Lang::code();
+    });
+    // current Language model instance
+    Route::get('/info/', function() {
+        return Lang::language();
+    });
+});
+
+// example of how to use multilingual prefixed routes
+// this will generate routes like: en/account/ , en/account/login
+Route::langGroup(array('prefix' => 'account'), function() {
+    Route::get('/', function() {
+        return 'Account home';
+    });
+    Route::get('/login/', function() {
+        return 'Login page';
+    });
 });
