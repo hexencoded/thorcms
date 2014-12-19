da3f22b23cc076bcca31835448b1b6f19e50a22c
Igor K <hexencoded@gmail.com>
2014-12-19 23:18:32 +0300

    Fix redirect error 404.

--- app/routes.php
+++ app/routes.php
@@ -12,27 +12,27 @@
  */
 
 // When visitors hit the root path, redirect them to the default language
-if(Config::get('thor:i18n.enabled')){
-    Route::get('/', function() {
-        return Redirect::to(Lang::code(), 302);
-    });
+if(Config::get('thor::i18n.enabled')) {
+	Route::get('/', function() {
+		return Redirect::to(Lang::code(), 302);
+	});
 }
 
 // All other multilingual routes
 Route::langGroup(function() {
-    // Multilingual home
-    Route::get('/', function() {
-        Doc::title('Thor CMS')->h1('Thor CMS')->error();
-        return View::make('home');
-    });
+	// Multilingual home
+	Route::get('/', function() {
+		Doc::title('Thor CMS')->h1('Thor CMS')->error();
+		return View::make('home');
+	});
 
-    // URL to Pageable resolver. Bust be after all other route definitions
-    Route::any('{slug}', array('before' => 'pageable.resolve', 'uses' => function($slug){
-        $pageable = Route::resolvePageable($slug);
-        if(($pageable instanceof \Thor\Models\Behaviours\IPageable) and ($pageable->exists())) {
-            return $pageable->execute(array('slug'=>$slug));
-        }else{
-            App::abort(404);
-        }
-    }))->where('slug', '.*');
+	// URL to Pageable resolver. Bust be after all other route definitions
+	Route::any('{slug}', array('before' => 'pageable.resolve', 'uses' => function($slug) {
+		$pageable = Route::resolvePageable($slug);
+		if(($pageable instanceof \Thor\Models\Behaviours\IPageable) and ($pageable->exists())) {
+			return $pageable->execute(array('slug'=>$slug));
+		} else {
+			App::abort(404);
+		}
+	}))->where('slug', '.*');
 });
